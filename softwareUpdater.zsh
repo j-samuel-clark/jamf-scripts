08845ba19035fb0e11eb6868a5976ea3639ebb24
Sam Clark <62838033+j-samuel-clark@users.noreply.github.com>
2020-10-07 10:45:07 +0200

    Update softwareUpdater.zsh

--- softwareUpdater.zsh
+++ softwareUpdater.zsh
@@ -29,15 +29,19 @@ swuIcon="/System/Library/CoreServices/Software Update.app/Contents/Resources/Sof
 ##########################################################################################
 function jamfHelper () {
 #Runs jamfhelper and tells the user to acknowldege the actions for reissuing a filevault key
+	echo "Showing the jamfHelper dialogue"
     "/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" \
     -windowType utility \
     -lockHUD \
     -title "Restart Required" \
+    -alignHeading center \
     -description \
-"Your computer must be restarted to finish updating. Please save your work and click \"Ok\" to restart." \
-    -alignDescription justified \
+"Your computer must be restarted to finish updating. Please save your work and click \"Restart\".
+
+Please allow up to 30 seconds for the restart to happen automatically." \
+    -alignDescription left \
     -icon "$swuIcon" \
-    -button1 "OK" \
+    -button1 "Restart" \
     -defaultButton 1
 }
 
@@ -52,6 +56,7 @@ function checkRestart() {
 #Checks which kind of restart should be taken
 	updateWithRestart=$(/usr/sbin/softwareupdate --list --all | grep "Action: restart")
 	if [[ -n "$updateWithRestart" ]]; then 
+		echo "A restart is required for this update"
 		restartRequired="Yes"
 		#Check if authenticated restart is possible
 		authRestart=$(`/usr/bin/fdesetup supportsauthrestart`)
@@ -63,12 +68,17 @@ function checkRestart() {
 
 function niceRestart () {
 #Restart the computer based on type
+	#Displaying restart dialogue
+	helperWindow
+
 	if [[ $authRestart == "Yes" ]]; then
-		sleep 2
+		echo "An authenticated restart will take place during the update"
+		sleep 5
 		/usr/bin/fdesetup authrestart
 	else
-		sleep 2
-		/usr/bin/osascript -e 'tell application "System Events" to restart'
+		echo "No authenticated restart is possible on this device"
+		sleep 5
+		shutdown -r now
 	fi
 }
 
@@ -78,55 +88,69 @@ function inventoryUpdater () {
 	local daemonLabel="com.$orgName.softwareupdate"
 
 	#Creating $orgNameFramework if not present
-	if [[ ! -d "$orgFramework" ]]; then
-		/bin/mkdir "$orgFramework"/bin
+	if [[ ! -d "$orgFramework" ]] || [[ ! -d ""orgFramework"/bin" ]]; then
+		echo "Creating the $orgName framework filepath"
+		/bin/mkdir -p "$orgFramework"/bin
 	fi
 
 	#Writing Launch Daemon
-	/bin/cat <<EOF > "$daemon"
-	<?xml version="1.0" encoding="UTF-8"?>
-	<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-	<plist version="1.0">
-	<dict>
-		<key>Label</key>
-		<string>com.$orgName.softwareupdate</string>
-		<key>RunAtLoad</key>
-		<true/>
-		<key>UserName</key>
-		<string>root</string>
-		<key>ProgramArguments</key>
-		<array>
-			<string>/bin/zsh</string>
-			<string>"$orgFramework"/bin/runRecon</string>
-			<string>recon</string>
-		</array>
-		<key>StandardErrorPath</key>
-		<string>/var/tmp/run.err</string>
-		<key>StandardOutPath</key>
-		<string>/var/tmp/run.out</string>
-	</dict>
-	</plist>
+	echo "Writing the softwareupdate daemon"
+/bin/cat <<EOF > $daemon
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Label</key>
+	<string>com.$orgName.softwareupdate</string>
+	<key>RunAtLoad</key>
+	<true/>
+	<key>UserName</key>
+	<string>root</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/bin/zsh</string>
+		<string>$orgFramework/bin/runRecon</string>
+		<string>recon</string>
+	</array>
+	<key>StandardErrorPath</key>
+	<string>/var/tmp/run.err</string>
+	<key>StandardOutPath</key>
+	<string>/var/tmp/run.out</string>
+</dict>
+</plist>
 EOF
 
-	/bin/cat <<EOF > "$orgFramework"/bin/runRecon
-	#!/bin/zsh
-	function waitForUser() {
-	    dockStatus=$( /usr/bin/pgrep -x Dock )
-	    while [[ "$dockStatus" == "" ]]; do
-	        sleep 2
-	        dockStatus=$( /usr/bin/pgrep -x Dock )
-	    done
-	    finderStatus=$(pgrep -l "Finder")
-	    until [ "$finderStatus" != "" ]; do
-	        sleep 10
-	        $finderStatus=$(pgrep -l "Finder")
-	    done
-	}
-	waitForUser
-	/usr/local/bin/jamf recon
-	/bin/rm -r $orgFramework/bin/runRecon
-	/bin/rm -r $daemon
-	/bin/launchctl remove $daemonLabel
+	#Writing runRecon executable
+	echo "Writing the runRecon executable"
+/bin/cat <<EOF > "$orgFramework"/bin/runRecon
+#!/bin/zsh
+function waitForUser() {
+    dockStatus=\$( /usr/bin/pgrep -x Dock )
+    while [[ "\$dockStatus" == "" ]]; do
+        sleep 2
+        dockStatus=\$( /usr/bin/pgrep -x Dock )
+    done
+    finderStatus=\$(pgrep -l "Finder")
+    until [ "\$finderStatus" != "" ]; do
+        sleep 10
+        \$finderStatus=\$(pgrep -l "Finder")
+    done
+}
+function waitForJSS() {
+	jssAvailable=\$(/usr/local/bin/jamf checkJssConnection | grep "available")
+	until [[ -n \$jssAvailable ]]; do
+		echo "The JSS is unavailable, waiting"
+		sleep 2
+		\$jssAvailable=\$(/usr/local/bin/jamf checkJssConnection | grep "available" )
+	done
+	echo "The JSS is available, moving on" 
+}
+waitForUser
+waitForJSS
+/usr/local/bin/jamf recon
+/bin/rm -r $orgFramework/bin/runRecon
+/bin/rm -r $daemon
+/bin/launchctl remove $daemonLabel
 EOF
 	#Setting permissions for daemon and runRecon
 	/bin/chmod 644 "$daemon"
@@ -147,9 +171,7 @@ checkRestart
 
 #If restart is needed, determinig which type of restart should be used
 if [[ $restartRequired == "Yes" ]]; then 
-	#Displaying restart dialogue
-	helperWindow
-
+	
 	#Creating the inventory updater daemon
 	inventoryUpdater
 	
